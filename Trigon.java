b0d6c27faf3405457cb4120b2f55383f331b5c2a
Dertanzer123 <131368871+Dertanzer123@users.noreply.github.com>
2024-03-11 00:21:34 +0300

    Update
    added new class

---
+++ Trigon.java
@@ -0,0 +1,29 @@
+include Vector;
+include Vertex;
+
+
+Trigon{
+
+Vertex v1;
+Vertex v2;
+Vertex v3;
+
+Vector3 normal;
+
+Trigon (Vertex v1,Vertex v2,Vertex v3){
+
+this.v1=v1;
+this.v2=v2;
+this.v3=v3;
+
+
+recalcnorm;
+
+}
+public void recalcnorm() 
+{
+
+}
+
+
+}
