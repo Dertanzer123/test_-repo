d4b49d3e34f6a71809c1430788f7a5739f5197b7
Dertanzer123 <131368871+Dertanzer123@users.noreply.github.com>
2024-03-11 00:39:14 +0300

    Update Vector.java

--- Vector.java
+++ Vector.java
@@ -5,16 +5,12 @@ public class Vector{
 
 
 public static Vector3 crossP(Vector3 in1,Vector3 in2){
-
-
 return new Vector3(in1.y*in2.z-in1.z*in2.y,in1.z*in2.x-in1.x*in2.z,in1.x*in2.y-in1.y*in2.x);
-
-
-
-
-
 }
-
+public static Vector3 Vsum(Vector3 in1, Vector3 in2) 
+{
+return new Vector3(in1.x+in2.x,in1.y+in2.y,in1.z+in2.z);
+}
 
 }
 class Vector3 {
