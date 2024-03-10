2f5ca098ddeec02592306e7f43e36bf746c51308
Dertanzer123 <131368871+Dertanzer123@users.noreply.github.com>
2024-03-11 00:28:19 +0300

    Update Vector.java
    added new crossP method

--- Vector.java
+++ Vector.java
@@ -4,7 +4,16 @@
 public class Vector{ 
 
 
+public static Vector3 crossP(Vector3 in1,Vector3 in2){
 
+
+return new Vector3(in1.y*in2.z-in1.z*in2.y,in1.z*in2.x-in1.x*in2.z,in1.x*in2.y-in1.y*in2.x);
+
+
+
+
+
+}
 
 
 }
@@ -20,7 +29,7 @@ this.x=x;
 this.y=y;
 this.z=z;
 }
-public lenght()
+public double lenght()
 {
 return Math.sqrt(x*x+y*y+z*z);
 }
