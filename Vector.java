334f0fc08374b11af001e6549bea120ecca8263d
Dertanzer123 <131368871+Dertanzer123@users.noreply.github.com>
2024-03-10 23:34:51 +0300

    Update
    creating vector class and defining basics

---
+++ Vector.java
@@ -0,0 +1,30 @@
+
+
+
+public class Vector{ 
+
+
+
+
+
+}
+class Vector3 {
+
+double x;
+double y;
+double z;
+
+Vector3(double x,double y,double z)
+{
+this.x=x;
+this.y=y;
+this.z=z;
+}
+public lenght()
+{
+return Math.sqrt(x*x+y*y+z*z);
+}
+
+
+
+}
